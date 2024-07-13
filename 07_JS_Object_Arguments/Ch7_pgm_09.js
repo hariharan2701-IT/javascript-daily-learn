90213f3d914766eef7cdc5666a9e7323d01cd57b
hariharan2701-IT <hariharan27012004@gmail.com>
2024-07-13 14:37:15 +0530

    mod: pgm 8and 9

--- 07_JS_Object_Arguments/Ch7_pgm_09.js
+++ 07_JS_Object_Arguments/Ch7_pgm_09.js
@@ -22,3 +22,16 @@ console.log(message.substr(charIndex, 3));
  *    'oo' in the word 'Moon' of the message string.
  *
  */
+
+//Using indexOf to Find "go"
+var message = "We choose to go to the Moon!";
+
+var startIndex = message.indexOf("go");
+console.log("Index of 'go':", startIndex);
+
+
+//Using indexOf and substr to Display "choose"
+var message = "We choose to go to the Moon!";
+
+var moonIndex = message.lastIndexOf("oo");
+console.log("Index of 'oo' in 'Moon':", moonIndex);
