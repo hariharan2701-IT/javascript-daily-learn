8cb34622980316488f949ace90ac730514a30a25
hariharan2701-IT <hariharan27012004@gmail.com>
2024-07-13 14:36:26 +0530

    fixed pgm 8

--- 07_JS_Object_Arguments/Ch7_pgm_08.js
+++ 07_JS_Object_Arguments/Ch7_pgm_08.js
@@ -22,3 +22,9 @@ console.log(message.substr(3, 12));
  *    as arguments?
  *
  */
+
+var message = "We choose to go to the Moon!";
+
+console.log(message.substr(20));     // Output: Moon!
+console.log(message.substr(11, 6));  // Output: go to the
+console.log(message.substr(0, 10));  // Output: We choose
