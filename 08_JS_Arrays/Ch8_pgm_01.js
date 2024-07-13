d7c9183a398af6ef5ee24792721a4e0c7c9bd6e0
hariharan2701-IT <hariharan27012004@gmail.com>
2024-07-13 17:28:16 +0530

    learned: JS conditions

--- 08_JS_Arrays/Ch8_pgm_01.js
+++ 08_JS_Arrays/Ch8_pgm_01.js
@@ -1,5 +1,5 @@
 // Creating arrays
-
+/*
 var scores;
 var names;
 
@@ -8,7 +8,7 @@ names = [ "Kandra", "Dax", "Blinky" ];
 
 console.log(scores);
 console.log(names);
-
+*/
 
 
 /* Further Adventures
@@ -23,3 +23,26 @@ console.log(names);
  * 4) Log akaScores to the console.
  *
  */
+
+// Creating arrays
+var scores;
+var names;
+
+scores = [ 3, 1, 8, 2 ];
+names = [ "Kandra", "Dax", "Blinky" ];
+
+// Add an extra element to each array
+scores.push(5); // Adding 5 to the scores array
+names.push("Zara"); // Adding "Zara" to the names array
+
+console.log(scores); // Output the scores array
+console.log(names); // Output the names array
+
+// Declare a third variable, akaScores
+var akaScores;
+
+// Assign the scores array to akaScores
+akaScores = scores;
+
+// Log akaScores to the console
+console.log(akaScores); // Output the akaScores array
--- 09_JS_Constructors/Ch9_pgm_05.js
+++ 09_JS_Constructors/Ch9_pgm_05.js
@@ -1,5 +1,5 @@
 // Including a moons array in our Planet constructor
-
+/*
 var Planet = function (name, position, type) {
     this.name = name;
     this.position = position;
@@ -42,3 +42,57 @@ planet.showPlanet();
  *    from the moons array.
  *
  */
+
+// Constructor function for Planet with moons array
+var Planet = function (name, position, type) {
+    this.name = name;
+    this.position = position;
+    this.type = type;
+    this.moons = [];
+
+    this.showPlanet = function () {
+        var info = this.name + ": planet " + this.position;
+        info += " - " + this.type;
+        console.log(info);
+        console.log("Moons: " + this.moons.join(', ') + ".");
+    };
+
+    this.addMoon = function (moon) {
+        this.moons.push(moon);
+    };
+
+    this.removeMoon = function () {
+        if (this.moons.length > 0) {
+            this.moons.pop();
+        } else {
+            console.log("No moons to remove.");
+        }
+    };
+};
+
+// Create the first planet (Jupiter) and add moons
+var planet1 = new Planet("Jupiter", 5, "Gas Giant");
+planet1.addMoon("Io");
+planet1.addMoon("Europa");
+
+// Display planet1 information including moons
+planet1.showPlanet();
+
+// Further Adventures
+
+// 1) Create a second planet
+var planet2 = new Planet("Earth", 3, "Terrestrial");
+
+// 2) Add three moons to the second planet
+planet2.addMoon("Luna");
+planet2.addMoon("Phobos");
+planet2.addMoon("Deimos");
+
+// 3) Call the showPlanet method on the second planet
+console.log("\nSecond Planet:");
+planet2.showPlanet();
+
+// 4) Add a removeMoon method that removes the last moon from the moons array
+planet2.removeMoon();
+console.log("\nAfter removing a moon:");
+planet2.showPlanet();
